01953b46eac766811784620fcc086615ecc26e35
Jessica Slusark <jslusark@c4a13c9.42berlin.de>
2024-05-13 15:52:34 +0200

    restoring files

---
+++ headers/ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/10 14:12:45 by jslusark          #+#    #+#             */
+/*   Updated: 2024/05/13 13:41:07 by jslusark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h>
+# include <limits.h>
+# include <unistd.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_putchar(char c);
+//other funcs
+
+#endif
